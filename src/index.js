828af8836520f6cd411d198fef5ef406673d1c7f
chenmz <392997520@qq.com>
2018-01-05 17:04:22 +0800

    增加了键盘方向键控制关联列表样式变化

--- src/index.js
+++ src/index.js
@@ -4,13 +4,21 @@ import {render} from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.css';
 import jsonp from 'jsonp';
 /*
+第一部分：实现基本的关联功能
     1.给input绑定值改变事件，当值发生改变时调用对应的监听函数
     2.获取到input框中的值，然后调用百度接口获取数据，并修改状态对象中words属性
     3.调用百度接口时，除了传统方法，还可以用jsonp模块的jsonp函数方法
     具体：npm install jsonp,然后调用即可！
     https://www.baidu.com/su?wd=a&cb=cb
     param是回调函数callback的名字,jsonp函数url参数不是用引号包裹的？？？？
-
+    4.
+第二部分：实现上下方向键控制关联列表的样式
+    (1)状态加上index (2)li加上触发类名 （3）input搜索框加上onKeyDown事件 
+    1.注意className那里需要加上空格！（条件运算符成立及会在后面加上另一个类名）
+    2.input的onkeydown事件
+    3.用箭头函数不用绑定this,事件函数一般以Handle开头
+    4.event.keyCode    向上：keyCode=38   向下：keyCode=40
+    5.上下箭头调整时，Input内的值也要跟着变 value={this.state.words[this.state.index]}
 */
 export default class Suggest extends Component{
     constructor(){
@@ -29,6 +37,20 @@ export default class Suggest extends Component{
             this.setState({words:data.s})
         })
     }
+    handleKeyDown=(event)=>{
+        let code = event.keyCode;
+        console.log(code);
+        //当按下的是向上或向下的箭头键时，触发
+        if(code == 38 || 40){
+            let index = this.state.index;
+            if(code == 38){
+                index--
+            }else if(code == 40){
+                index++
+            }
+            this.setState({index:index})
+        }
+    }
     render(){
         return(
             <div className='container'>
@@ -36,13 +58,13 @@ export default class Suggest extends Component{
                     <div className='col-sm-8 col-sm-offset-2'>
                         <div className='panel panel-default'>
                             <div className='panel-heading'>
-                                <input type='text' className='form-control' onChange={this.handleChange}/>
+                                <input onKeyDown={this.handleKeyDown} value={this.state.words[this.state.index]} type='text' className='form-control' onChange={this.handleChange}/>
                             </div>
                             <div className='panel-body'>
                                 <ul className='list-group'>
                                     {
                                         this.state.words.map((word,index)=>
-                                            <li key={index} className='list-group-item'>{word}</li>
+                                            <li key={index} className={'list-group-item '+(index===this.state.index?'active':'')}>{word}</li>
                                         )
                                     }
                                 </ul>
